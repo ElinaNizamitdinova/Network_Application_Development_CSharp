f7ff3f21644aa9921ad62906921e3c6dd7afd84b
Elina <elina.eldarovna.spb@gmail.com>
2024-07-21 12:48:16 +0300

    Server

---
+++ Network_Application_Development_C#/Message.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Network_Application_Development_C_
+{
+    public class Message
+    {
+        public string Text { get; set; }
+        public DateTime  DateTime { get; set; }
+        public string NicknameFrom { get; set; }
+        public string NicknameTo { get; set; }
+        public string SerializeMessageToJson()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        public static Message? DeserializeMessageFromJson(string json) => JsonSerializer.Deserialize<Message>(json);
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.DateTime} Received a message from{this.NicknameFrom}:" +
+                $"{this.Text}");        }
+
+        public override string ToString()
+        {
+            return $"{this.DateTime}Received a message from{this.NicknameFrom}:" +
+                $"{this.Text}";
+        }
+    }
+
+
+}
---
+++ Network_Application_Development_C#/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Net.Security;
+
+
+namespace Network_Application_Development_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Message message = new Message() { Text = "Hi", DateTime = DateTime.Now, NicknameFrom = "Amy", NicknameTo = "All" };
+            string json = message.SerializeMessageToJson();
+            Console.WriteLine(json);
+            Message? msgDeserialize = Message.DeserializeMessageFromJson(json);
+            Server("Hello");
+        }
+
+
+
+        public static void Server(string name)
+        {
+           
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint IPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            
+            
+            Console.WriteLine("Server is waiting");
+           
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref IPEndPoint);
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                ThreadPool.QueueUserWorkItem(obj => {
+                    Message message = Message.DeserializeMessageFromJson(messageText);
+                    message.Print();
+
+                    byte[] reply = Encoding.UTF8.GetBytes("Сообщение получено");
+                    udpClient.Send(reply,reply.Length,IPEndPoint);
+                });
+
+            }
+            
+        }
+    }
+        
+}
