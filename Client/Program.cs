9b699333cbcec489f6208c90d3d2223ef2bcf117
Elina <elina.eldarovna.spb@gmail.com>
2024-07-22 11:28:16 +0300

    Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,43 +9,51 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
-            {
+            SentMessage(args[0]);
 
-                SentMessage("Liza",i);
-            }
+            //new Task(() => SentMessage(args[0])).RunSynchronously();
+
+            //Console.ReadKey();
 
         }
 
-        public static void SentMessage(string From, int i, string ip = "127.0.0.1")
+        public static void SentMessage(string From, string ip = "127.0.0.1")
         {
-            //   Message message = new Message() { Text = text, DateTime = DateTime.Now, NicknameFrom = From, NicknameTo = "Server" };
+            //Message message = new Message() { Text = text, DateTime = DateTime.Now, NicknameFrom = From, NicknameTo = "Server" };
             UdpClient udpClient = new UdpClient();
             IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ip), 12345);
-            //  string json = message.SerializeMessageToJson();
+            //string json = message.SerializeMessageToJson();
 
-           
-            
+            bool fl= true;
+            while (fl)
+            {
+                string messageText;
+                do
+                {
+                    Console.WriteLine("Enter your message");
+                    messageText = Console.ReadLine();
+                    if (messageText.ToLower() == "exit")
+                    {
+                        Console.WriteLine("Shutdown");
+                        fl = false;
+                        Console.ReadKey();
+                    }
+                }
+                while (string.IsNullOrEmpty(messageText));
 
-                string messageText = "Hello"+i;
-                //do
-                //{
-                //    Console.WriteLine("Enter your message");
-                //    messageText = Console.ReadLine();
-                //}
-                //while (string.IsNullOrEmpty(messageText));
                 Message message = new Message() { Text = messageText, DateTime = DateTime.Now, NicknameFrom = From, NicknameTo = "Server" };
                 string json = message.SerializeMessageToJson();
+
 
 
                 byte[] data = Encoding.UTF8.GetBytes(json);
                 udpClient.Send(data, data.Length, iPEndPoint);
 
-                byte[] buffer = udpClient.Receive(ref iPEndPoint);
-                var answer = Encoding.UTF8.GetString(buffer);
-                Console.WriteLine(answer);
-            
+                //byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                //var answer = Encoding.UTF8.GetString(buffer);
+                //Console.WriteLine(answer);
 
+            }
         }
     }
 }
